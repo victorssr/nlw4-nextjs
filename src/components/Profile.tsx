f4ed900fc5c03a44bc543156e48317348fa37c9d
Victor Savoi <savoi.victor@hotmail.com>
2021-02-25 22:04:29 -0300

    Challenge box aded

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/victorssr.png" alt="Victor Savoi" />
@@ -9,7 +13,7 @@ export function Profile() {
                 <strong>Victor Savoi</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
-                    Level 1                    
+                    Level {level}
                 </p>
             </div>
         </div>
